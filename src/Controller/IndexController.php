9358cca5befa484c4d3bf8c85cbd594990668d51
Vineet dhiman <vineetdhiman@Vineets-MacBook-Air.local>
2022-12-23 23:11:45 +0530

    read, edit, new

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,41 +11,121 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", "index")
+     * @Route("/", name="index")
      * @Method({"GET"})
      */
     public function index(ManagerRegistry $doctrine, Request $request): Response
     {
         $articles = $doctrine->getManager()->getRepository(Article::class)->findAll();
         
-        $this->addFlash(
-            'errors',
-            'Your changes were saved!'
-        );
         return $this->render('/article/index.html.twig', ['articles' => $articles]);
     }
 
     /**
-     * @Route("/article/save")
+     * @Route("/article/new")
      */
-    public function save(ManagerRegistry $doctrine)
+    public function new(ManagerRegistry $doctrine, Request $request)
     {
-        $entityManager = $doctrine->getManager();
-        $articleRepository = $entityManager->getRepository(Article::class);
-
         $article = new Article();
-        $article->setTitle('First Title 2');
-        $article->setBody('First Body 2');
-        $article->setCreatedAt(Carbon::now());
 
-        $articleRepository->add($article, true);
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control w-25 my-2']
+            ])
+            ->add('body', TextareaType::class, [
+                'attr' => ['class' => 'form-control w-25 my-2']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save',
+                'attr' => ['class' => 'btn btn-primary my-2']
+            ])
+            ->getForm();
 
-        return new Response('Added article ' . $article->getId());
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $article = $form->getData();
+                $article->setCreatedAt(Carbon::now());
+                $manager = $doctrine->getManager()->getRepository(Article::class);
+                $manager->add($article, true);
+                
+                $this->addFlash('notice', 'Article added successfully');
+                return $this->redirectToRoute('index');
+            }
+
+        return $this->render('/article/edit.html.twig', [
+            'form' => $form->createView(),
+            'title' => $article->getId() ? 'Edit' : 'New'
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}")
+     */
+    public function view(ManagerRegistry $doctrine, Request $request)
+    {
+        $article = $doctrine->getManager()->getRepository(Article::class)->find($request->get('id'));
+        return $this->render('/article/view.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit_article")
+     * @Method({"GET", "POST"})
+     */
+    public function edit(ManagerRegistry $doctrine, Request $request)
+    {
+        $article = $doctrine->getManager()->getRepository(Article::class)->find($request->get('id'));
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control w-25 my-2']
+            ])
+            ->add('body', TextareaType::class, [
+                'attr' => ['class' => 'form-control w-25 my-2']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save',
+                'attr' => ['class' => 'btn btn-primary my-2']
+            ])
+            ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $article = $form->getData();
+                $respository = $doctrine->getManager()->getRepository(Article::class);
+                $respository->add($article, true);
+                
+                $this->addFlash('notice', 'Article updated successfully');
+                return $this->redirectToRoute('index');
+            }
+
+        return $this->render('/article/edit.html.twig', [
+            'form' => $form->createView(),
+            'title' => $article->getId() ? 'Edit' : 'New'
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}")
+     * @Method()
+     */
+    public function delete(ManagerRegistry $doctrine, Request $request)
+    {
+        $id = $request->get('id');
+        $respository = $doctrine->getManager()->getRepository(Article::class);
+        $article = $respository->find((int) $id);
+        $respository->remove($article, true);
+
+        $this->addFlash('notice', 'Article deleted successfully');
+
+        return $this->redirectToRoute('index');
     }
 }
